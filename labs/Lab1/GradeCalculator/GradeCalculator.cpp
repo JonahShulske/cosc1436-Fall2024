0a97345e988e88afd5218c49dec929ce3c18a924
Jonah Shulske <jonah.shulske@my.tccd.edu>
2024-09-10 08:52:12 -0500

    Worked on Lab1

---
+++ labs/Lab1/GradeCalculator/GradeCalculator.cpp
@@ -0,0 +1,31 @@
+/* Lab 1
+* Jonah Shulske
+* COSC-1436 Fall 2024 
+*/
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string userName;
+    std::cout << "Please enter your name: ";
+    std::cin >> userName;
+    std::cout << "Hello, " << userName << std::endl;
+
+    double labGrade1, labGrade2, labGrade3, labGrade4;
+    double examGrade1, examGrade2, examGrade3;
+    //::cout << "Please enter your four Lab Grades: ";
+    std::cout << "Please enter Lab Grade #1: ";
+    std::cout << "Please enter Lab Grade #2: ";
+    /*std::cout << "Please enter Lab Grade #3: ";
+    std::cout << "Please enter Lab Grade #4: ";*/
+    /*std::cout << "Please enter Exam Grade #1: ";
+    std::cout << "Please enter Exam Grade #2: ";
+    std::cout << "Please enter Exam Grade #3: ";*/
+    //std::cin >> labGrade1, labGrade2, labGrade3, labGrade4;
+    //std::cin >> examGrade1, examGrade2, examGrade3;
+    std::cin >> labGrade1, labGrade2;
+
+
+}
