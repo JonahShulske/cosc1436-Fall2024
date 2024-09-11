ebe3e42842468dbbbdae0e59780200f2c4d4d614
Jonah Shulske <jonah.shulske@my.tccd.edu>
2024-09-11 08:30:12 -0500

    Added more onto Lab1 assignment, including fixing user input, as well as calculating averages.

--- labs/Lab1/GradeCalculator/GradeCalculator.cpp
+++ labs/Lab1/GradeCalculator/GradeCalculator.cpp
@@ -15,17 +15,35 @@ int main()
 
     double labGrade1, labGrade2, labGrade3, labGrade4;
     double examGrade1, examGrade2, examGrade3;
-    //::cout << "Please enter your four Lab Grades: ";
+
     std::cout << "Please enter Lab Grade #1: ";
+    std::cin >> labGrade1;
     std::cout << "Please enter Lab Grade #2: ";
-    /*std::cout << "Please enter Lab Grade #3: ";
-    std::cout << "Please enter Lab Grade #4: ";*/
-    /*std::cout << "Please enter Exam Grade #1: ";
-    std::cout << "Please enter Exam Grade #2: ";
-    std::cout << "Please enter Exam Grade #3: ";*/
-    //std::cin >> labGrade1, labGrade2, labGrade3, labGrade4;
-    //std::cin >> examGrade1, examGrade2, examGrade3;
-    std::cin >> labGrade1, labGrade2;
+    std::cin >> labGrade2;                          //Prompting user for Lab Grades
+    std::cout << "Please enter Lab Grade #3: ";
+    std::cin >> labGrade3;
+    std::cout << "Please enter Lab Grade #4: ";
+    std::cin >> labGrade4;
+
+    std::cout << "\n";
+
+    std::cout << "Please enter Exam Grade #1: ";
+    std::cin >> examGrade1;
+    std::cout << "Please enter Exam Grade #2: ";    //Prompting user for Exam Grades
+    std::cin >> examGrade2;
+    std::cout << "Please enter Exam Grade #3: ";
+    std::cin >> examGrade3;
+
+    double labAverage;
+    labAverage = ((labGrade1 + labGrade2 + labGrade3 + labGrade4) / 4);     //Calculation for Lab Grade Average
+    std::cout << labAverage << std::endl;
+
+    double examAverage;
+    examAverage = ((examGrade1 + examGrade2 + examGrade3) / 3);     //Calculation for Exam Grade Average
+    std::cout << examAverage;
+
+    /*std::cout << labGrade1 << " " << labGrade2 << " " << labGrade3 << " " << labGrade4 << "\n";
+    std::cout << examGrade1 << " " << examGrade2 << " " << examGrade3;*/
 
 
 }
