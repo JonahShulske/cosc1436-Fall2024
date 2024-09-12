47702b08bbc870de22798356fe3bdfc0ffdd64fb
Jonah Shulske <jonah.shulske@my.tccd.edu>
2024-09-12 08:53:32 -0500

    Almost finished with Lab1 assignment. May need some extra cleaning up, however.

--- labs/Lab1/GradeCalculator/GradeCalculator.cpp
+++ labs/Lab1/GradeCalculator/GradeCalculator.cpp
@@ -1,62 +1,82 @@
 /* Lab 1
-* Jonah Shulske
-* COSC-1436 Fall 2024 
+ * Jonah Shulske
+ * COSC-1436 Fall 2024 
 */
 
 #include <iostream>
 #include <string>
+#include <iomanip>
+
+using namespace std; //Only using std:: for when I'm actually printing lines so that I can tell it's something the user will see.
 
 int main()
 {
-    std::string userName;
+    string userName;
     std::cout << "Please enter your name: ";
-    std::getline(std::cin, userName);
-    //std::cin >> userName;
-   // std::cout << "Hello, " << userName << std::endl;
+    getline(cin, userName);
 
     double labGrade1, labGrade2, labGrade3, labGrade4;
-    double examGrade1, examGrade2, examGrade3;
+    double examGrade1, examGrade2, examGrade3;      //Creating main grade expressions for user to input
+    double partiGrade, finalExam;
 
     std::cout << "Please enter Lab Grade #1: ";
-    std::cin >> labGrade1;
+    cin >> labGrade1;
     std::cout << "Please enter Lab Grade #2: ";
-    std::cin >> labGrade2;                          //Prompting user for Lab Grades
+    cin >> labGrade2;                          //Prompting user for Lab Grades
     std::cout << "Please enter Lab Grade #3: ";
-    std::cin >> labGrade3;
+    cin >> labGrade3;
     std::cout << "Please enter Lab Grade #4: ";
-    std::cin >> labGrade4;
+    cin >> labGrade4;
 
-    std::cout << "\n";
+    cout << "\n";
 
     std::cout << "Please enter Exam Grade #1: ";
-    std::cin >> examGrade1;
+    cin >> examGrade1;
     std::cout << "Please enter Exam Grade #2: ";    //Prompting user for Exam Grades
-    std::cin >> examGrade2;
+    cin >> examGrade2;
     std::cout << "Please enter Exam Grade #3: ";
-    std::cin >> examGrade3;
+    cin >> examGrade3;
 
-    std::cout << "\n";
+    cout << "\n";
 
-    std::cout << userName << std::endl;
+    std::cout << "Please enter Participation Grade: ";     //Prompting user for Participation Grade
+    cin >> partiGrade;
 
-    std::cout << "\n";
+    cout << "\n";
+
+    std::cout << "Please enter Final Exam Grade: ";    //Prompting user for Final Exam Grade
+    cin >> finalExam;
+
+    cout << "\n";
 
     double labAverage;
+    std::cout << userName << ", " << "your lab grades are: " << endl;
     labAverage = ((labGrade1 + labGrade2 + labGrade3 + labGrade4) / 4);     //Calculation for Lab Grade Average
-    std::cout << "Lab #1 Grade: " << labGrade1 << "\n" << "Lab #2 Grade: " << labGrade2 << std::endl;
-    std::cout << "Lab #3 Grade: " << labGrade3 << "\n" << "Lab #4 Grade: " << labGrade4 << std::endl;
-    std::cout << "Your average lab grade is: "<< labAverage << std::endl;
+    std::cout << "Lab #1 Grade: " << labGrade1 << "\n" << "Lab #2 Grade: " << labGrade2 << endl;
+    std::cout << "Lab #3 Grade: " << labGrade3 << "\n" << "Lab #4 Grade: " << labGrade4 << endl;
 
-    std::cout << "\n";
+    cout << "\n";
 
     double examAverage;
+    std::cout << userName << ", " << "your exam grades are: " << endl;
     examAverage = ((examGrade1 + examGrade2 + examGrade3) / 3);     //Calculation for Exam Grade Average
-    std::cout << "Exam #1 Grade: " << examGrade1 << "\n" << "Exam #2 Grade: " << examGrade2 << std::endl;
-    std::cout << "Exam #3 Grade: " << examGrade3 << std::endl;
-    std::cout << "Your average exam grade is: " << examAverage;
+    std::cout << "Exam #1 Grade: " << examGrade1 << "\n" << "Exam #2 Grade: " << examGrade2 << endl;
+    std::cout << "Exam #3 Grade: " << examGrade3 << endl;
 
-    /*std::cout << labGrade1 << " " << labGrade2 << " " << labGrade3 << " " << labGrade4 << "\n";
-    std::cout << examGrade1 << " " << examGrade2 << " " << examGrade3;*/
+    cout << "\n";
 
+    std::cout << userName << ", " << "your other grades are: " << endl;
+    std::cout << "Participation Grade: " << partiGrade << endl;     //Simply printing the user's other grades
+    std::cout << "Final Exam Grade: " << finalExam << endl;
+
+    cout << "\n";
+
+    double classAverage = ((labAverage + examAverage + partiGrade + finalExam) / 4);    //Calculation for Class Average grade
+    cout << fixed << setprecision(2);
+    std::cout << "Lab Average (65%): " << labAverage << endl;
+    std::cout << "Exam Average (20%): " << examAverage << endl;
+    std::cout << "Participation (5%): " << partiGrade << endl;
+    std::cout << "Final Exam (10%): " << finalExam << endl;
+    std::cout << "Class Average: " << classAverage << endl;
 
 }
