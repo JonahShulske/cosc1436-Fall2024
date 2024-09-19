623d3a1d4ae74386ced3902f4d77fc7b3d79d476
Jonah Shulske <jonah.shulske@my.tccd.edu>
2024-09-18 21:29:41 -0500

    Added onto Chapter 4 notes

--- classwork/Chapter4/MovieLibrary/MovieLibrary.cpp
+++ classwork/Chapter4/MovieLibrary/MovieLibrary.cpp
@@ -1,6 +1,6 @@
 /* Movie Library
  * Jonah Shulske
- * 9/16/24
+ * 9/16/24 + 9/18/24
  */
 
 #include <iostream>
@@ -58,6 +58,7 @@ int main()
   //   * left == right | 0
   //   * left > right | <0
   //   */
+  // 
   //  cout << left << " ciequal " << right << " = " << _strcmpi(left.c_str(), right.c_str()) << endl;
   //  cout << left << " < " << right << " = " << (left < right) << endl;
   //  cout << left << " <= " << right << " = " << (left <= right) << endl;
@@ -67,9 +68,13 @@ int main()
   //  cout << left << " != " << right << " = " << (left != right) << endl;
   //  cout << endl;
 
-    
+
     /* Decision Making - If Statement
-     * if-stmt::= 
+     * if-stmt::=
+     * elses::stmt=else
+     * logical_op::&& AND (Binary operator) Both must be true for && to return true. If one is false, then && returns false
+     * logical_op::|| OR  (Binary operator) One or the other can be true/false, and it will return true/false.
+     * !Eb will give the opposite of whichever the operator returns
      */
 
     ////// Add a new movie
@@ -88,8 +93,110 @@ int main()
     };
 
 
-    ////// Display movie details
-    cout << "----------" << endl;
-    std::cout << movie.Title << endl;
+    // Get run length, at least 0 minutes
+    std::cout << "Enter run length in minutes: ";
+    cin >> movie.runLength;
 
+    //if (movie.runLength <= 0)
+    //{
+
+    //    // Look at additional things...
+
+    //    if (movie.runLength < 0) 
+    //    {
+    //        std::cout << "ERROR: Run Length must be at least 0 minutes" << endl;
+
+    //        std::cout << "Enter run length in minutes: ";
+    //        cin >> movie.runLength;
+    //    }
+    //} else  if (movie.runLength >= 1440)
+    //{
+    //    std::cout << "ERROR: Run Length cannot be over one day long" << endl;
+
+    //    std::cout << "Enter run length in minutes: ";
+    //    cin >> movie.runLength;
+    //}
+
+    if (movie.runLength < 0 || movie.runLength > 1440)
+    {
+        std::cout << "Error: Run length must be between 0 - 1440 minutes" << endl;
+
+        std::cout << "Enter run length in minutes: ";
+        cin >> movie.runLength;
+    }
+
+    // Get release year
+    std::cout << "Enter movie release year (1900-2100): ";
+    cin >> movie.releaseYear;
+
+    if (movie.releaseYear < 1900 || movie.releaseYear > 2100)
+    {
+        std::cout << "ERROR: Movie release year must range from the years 1900-2100" << endl;
+
+        std::cout << "Enter movie release year (1900-2100) ";
+        cin >> movie.releaseYear;
+    }
+
+    /*if (movie.releaseYear < 1900)
+    {
+        std::cout << "ERROR: movie release year must be after the year 1900" << endl;
+
+        std::cout << "Enter movie release year (1900+): ";
+        cin >> movie.releaseYear;
+    }*/
+
+
+    // Get the optional description
+    std::cout << "Enter the optional description: ";
+    cin.ignore();
+    getline(cin, movie.Description);
+
+    // Get Is Classic
+    char isClassic;
+    std::cout << "Is this a classic? (Y/N) ";
+    cin >> isClassic;
+
+
+    //if (isClassic == 'Y')
+    //    movie.isClassic = true;
+    //else if (isClassic == 'y')
+    //    movie.isClassic = true;
+    //else if (isClassic == 'N')
+    //    movie.isClassic = false;
+    //else if (isClassic == 'n')
+    //    movie.isClassic = false;
+
+    if (isClassic == 'Y' || isClassic == 'y')
+        movie.isClassic = true;
+    else if (isClassic == 'N' || isClassic == 'n')
+        movie.isClassic = false;
+    else
+    {
+        std::cout << "ERROR: Enter (Y/N) characters only" << endl;
+
+        std::cout << "Is this a classic? (Y/N) ";
+        cin >> isClassic;
+    }
+
+    ///*if (isClassic == 'Y')
+    //    movie.isClassic = true;
+    //if (isClassic == 'y')
+    //    movie.isClassic = true;
+    //if (isClassic == 'N')
+    //    movie.isClassic = false;
+    //if (isClassic == 'n')
+    //    movie.isClassic = false;*/
+
+    ////// Display movie details
+
+    std::cout << "---------------" << endl;
+    std::cout << movie.Title << " (" << movie.releaseYear << ")" << endl;
+    std::cout << "Run Length in minutes: " << movie.runLength << endl;
+    if (movie.isClassic)
+        std::cout << "Is it a Classic? Yes" << endl;
+    else
+        std::cout << "Is it a Classic? No" << endl;
+    if (movie.Description != "")
+        std::cout << movie.Description << endl;
+    std::cout << "---------------" << endl;
 }
