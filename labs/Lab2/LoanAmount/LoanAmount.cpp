529a2580887a41077dd1ffddafa958b73f960578
Jonah Shulske <jonah.shulske@my.tccd.edu>
2024-09-30 12:28:17 -0500

    Trying to construct a bank to create loans. Cannot figure out why loop does not work, and why I'm getting errors.

--- labs/Lab2/LoanAmount/LoanAmount.cpp
+++ labs/Lab2/LoanAmount/LoanAmount.cpp
@@ -8,52 +8,80 @@
 #include <cmath>
 #include <string>
 
+
+struct Bank
+{
+    // Loan Amount
+    int loanAmount;
+
+    // Interest Rate
+    double interestRate;
+
+    // Payment Amount
+    double paymentAmount;
+
+};
+
 using namespace std;
 
 int main()
 {
-    int loanAmount;
+    //int loanAmount;
 
-    std::cout << "Please Enter Loan Amount ($1-$1,000): ";
-    cin >> loanAmount;
+    /*std::cout << "Please Enter Loan Amount ($1-$1,000): ";
+    cin >> loanAmount;*/
 
-    if (loanAmount > 1000 || loanAmount < 0)
+    Bank bank;
+
+    while (bank.loanAmount < 0 || bank.loanAmount > 1000)
+    {
+        std::cout << "Please Enter Loan Amount ($1-$1,000): ";
+        cin >> bank.loanAmount;
+
+        if (bank.loanAmount < 0 || bank.loanAmount > 1000)
+        {
+            std::cout << "ERROR: Loan Amount must be between $1-$1,000" << endl;
+        }
+
+    };
+
+    /*if (loanAmount > 1000 || loanAmount < 0)
     {
         std::cout << "ERROR: Loan Amount must be between $1-$1,000" << endl;
         std::cout << "Please Enter Loan Amount ($1-$1,000): ";
         cin >> loanAmount;
-    }
+    }*/
 
-    double interestRate;
+    //double interestRate;
 
     std::cout << "Please Enter Desired Interest Rate: (1.0%-100.0%): ";
-    cin >> interestRate;
+    cin >> bank.interestRate;
 
-    if (interestRate > 100.0 || interestRate < 1.0)
+    if (bank.interestRate > 100.0 || bank.interestRate < 1.0)
     {
         std::cout << "ERROR: Interest Rate must be between 1.0%-100.0%" << endl;
         std::cout << "Please Enter Desired Interest Rate: (1.0%-100.0%): ";
-        cin >> interestRate;
+        cin >> bank.interestRate;
     }
 
-    int paymentAmount;
+    //int paymentAmount;
     std::cout << "Please Enter Payment Amount ($1 - Loan Amount $): ";
-    cin >> paymentAmount;
+    cin >> bank.paymentAmount;
 
-    interestRate = (interestRate / 100);
+    bank.interestRate = (bank.interestRate / 100);
 
-    if (paymentAmount < 1 || paymentAmount > loanAmount)
+    if (bank.paymentAmount < 1 || bank.paymentAmount > bank.loanAmount)
     {
         std::cout << "ERROR: Payment must be above $1, but below Loan Amount" << endl;
         std::cout << "Please Enter Payment Amount ($1 - Loan Amount $): ";
-        cin >> paymentAmount;
+        cin >> bank.paymentAmount;
     }
 
-    double newBalance = (loanAmount - paymentAmount) + (loanAmount * interestRate);
+    double newBalance = (bank.loanAmount - bank.paymentAmount) + (bank.loanAmount * bank.interestRate);
 
-    std::cout << loanAmount << endl;
-    std::cout << interestRate << endl;
-    std::cout << paymentAmount << endl;
+    std::cout << bank.loanAmount << endl;
+    std::cout << bank.interestRate << endl;
+    std::cout << bank.paymentAmount << endl;
     std::cout << newBalance;
 
    /* std::cout << left << setw(6) << "Month" << setw(8) << "Balance" << setw(8) << "Payment" << setw(9) << "Interest" << setw(11) << "New Balance" << endl;
