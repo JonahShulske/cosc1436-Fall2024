fc304f1fc1948e22f309722e6a8b738a235a3231
Jonah Shulske <jonah.shulske@my.tccd.edu>
2024-09-30 19:05:45 -0500

    Figured out the issue with my loops.

--- labs/Lab2/LoanAmount/LoanAmount.cpp
+++ labs/Lab2/LoanAmount/LoanAmount.cpp
@@ -9,79 +9,79 @@
 #include <string>
 
 
-struct Bank
-{
-    // Loan Amount
-    int loanAmount;
-
-    // Interest Rate
-    double interestRate;
-
-    // Payment Amount
-    double paymentAmount;
-
-};
+//struct Bank
+//{
+//    // Loan Amount
+//    int loanAmount;
+//
+//    // Interest Rate
+//    double interestRate;
+//
+//    // Payment Amount
+//    double paymentAmount;
+//
+//};
 
 using namespace std;
 
 int main()
 {
-    //int loanAmount;
+    int loanAmount;
 
-    /*std::cout << "Please Enter Loan Amount ($1-$1,000): ";
-    cin >> loanAmount;*/
+    std::cout << "Please Enter Loan Amount ($1-$1,000): ";
+    cin >> loanAmount;
 
-    Bank bank;
-
-    while (bank.loanAmount < 0 || bank.loanAmount > 1000)
+    while (loanAmount < 1 || loanAmount > 1000)
     {
-        std::cout << "Please Enter Loan Amount ($1-$1,000): ";
-        cin >> bank.loanAmount;
 
-        if (bank.loanAmount < 0 || bank.loanAmount > 1000)
-        {
-            std::cout << "ERROR: Loan Amount must be between $1-$1,000" << endl;
-        }
+       if (loanAmount < 1 || loanAmount > 1000)
+       {
+          std::cout << "ERROR: Loan Amount must be between $1-$1,000" << endl;
+          std::cout << "Please Enter Loan Amount ($1-$1,000): ";
+          cin >> loanAmount;
+       }
 
     };
 
-    /*if (loanAmount > 1000 || loanAmount < 0)
-    {
-        std::cout << "ERROR: Loan Amount must be between $1-$1,000" << endl;
-        std::cout << "Please Enter Loan Amount ($1-$1,000): ";
-        cin >> loanAmount;
-    }*/
-
-    //double interestRate;
+    double interestRate;
 
     std::cout << "Please Enter Desired Interest Rate: (1.0%-100.0%): ";
-    cin >> bank.interestRate;
+    cin >> interestRate;
 
-    if (bank.interestRate > 100.0 || bank.interestRate < 1.0)
+    while (interestRate < 1.0 || interestRate > 100.0)
     {
-        std::cout << "ERROR: Interest Rate must be between 1.0%-100.0%" << endl;
-        std::cout << "Please Enter Desired Interest Rate: (1.0%-100.0%): ";
-        cin >> bank.interestRate;
-    }
 
-    //int paymentAmount;
-    std::cout << "Please Enter Payment Amount ($1 - Loan Amount $): ";
-    cin >> bank.paymentAmount;
+        if (interestRate > 100.0 || interestRate < 1.0)
+        {
+            std::cout << "ERROR: Interest Rate must be between 1.0%-100.0%" << endl;
+            std::cout << "Please Enter Desired Interest Rate: (1.0%-100.0%): ";
+            cin >> interestRate;
+        }
+    };
 
-    bank.interestRate = (bank.interestRate / 100);
+    interestRate = (interestRate / 100);
 
-    if (bank.paymentAmount < 1 || bank.paymentAmount > bank.loanAmount)
+    int paymentAmount;
+    std::cout << "Please Enter Payment Amount ($1 - $" << loanAmount << "): ";
+    cin >> paymentAmount;
+
+
+    while (paymentAmount < 1 || paymentAmount > loanAmount)
     {
-        std::cout << "ERROR: Payment must be above $1, but below Loan Amount" << endl;
-        std::cout << "Please Enter Payment Amount ($1 - Loan Amount $): ";
-        cin >> bank.paymentAmount;
-    }
 
-    double newBalance = (bank.loanAmount - bank.paymentAmount) + (bank.loanAmount * bank.interestRate);
+        if (paymentAmount < 1 || paymentAmount > loanAmount)
+        {
+            std::cout << "ERROR: Payment must be above $1, but below Loan Amount" << endl;
+            std::cout << "Please Enter Payment Amount ($1 - $" << loanAmount << "): ";
+            cin >> paymentAmount;
+        }
+    };
 
-    std::cout << bank.loanAmount << endl;
-    std::cout << bank.interestRate << endl;
-    std::cout << bank.paymentAmount << endl;
+    double newBalance = (loanAmount - paymentAmount) + (loanAmount * interestRate);
+
+    std::cout << loanAmount << endl;
+    std::cout << interestRate << endl;
+    std::cout << paymentAmount << endl;
     std::cout << newBalance;
 
    /* std::cout << left << setw(6) << "Month" << setw(8) << "Balance" << setw(8) << "Payment" << setw(9) << "Interest" << setw(11) << "New Balance" << endl;
