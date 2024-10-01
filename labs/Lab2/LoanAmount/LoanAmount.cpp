3510d15c445a221500cbc019df25b684ba95d43c
Jonah Shulske <jonah.shulske@my.tccd.edu>
2024-10-01 09:05:34 -0500

    Trying to create math loop for loan balances.

--- labs/Lab2/LoanAmount/LoanAmount.cpp
+++ labs/Lab2/LoanAmount/LoanAmount.cpp
@@ -26,7 +26,7 @@ using namespace std;
 
 int main()
 {
-    int loanAmount;
+    double loanAmount;
 
     std::cout << "Please Enter Loan Amount ($1-$1,000): ";
     cin >> loanAmount;
@@ -61,44 +61,60 @@ int main()
 
     interestRate = (interestRate / 100);
 
-    int paymentAmount;
-    std::cout << "Please Enter Payment Amount ($1 - $" << loanAmount << "): ";
+    double paymentAmount;
+    std::cout << "Please Enter Monthly Payment Amount ($0 - $" << loanAmount << "): ";
     cin >> paymentAmount;
 
 
-    while (paymentAmount < 1 || paymentAmount > loanAmount)
+    while (paymentAmount < 0 || paymentAmount > loanAmount)
     {
 
-        if (paymentAmount < 1 || paymentAmount > loanAmount)
+        if (paymentAmount < 0 || paymentAmount > loanAmount)
         {
-            std::cout << "ERROR: Payment must be above $1, but below Loan Amount" << endl;
-            std::cout << "Please Enter Payment Amount ($1 - $" << loanAmount << "): ";
+            std::cout << "ERROR: Payment must be between $0 - $" << loanAmount << endl;
+            std::cout << "Please Enter Monthly Payment Amount ($0 - $" << loanAmount << "): ";
             cin >> paymentAmount;
         }
     };
 
-    double newBalance = (loanAmount - paymentAmount) + (loanAmount * interestRate);
 
     std::cout << loanAmount << endl;
     std::cout << interestRate << endl;
     std::cout << paymentAmount << endl;
-    std::cout << newBalance;
 
-   /* std::cout << left << setw(6) << "Month" << setw(8) << "Balance" << setw(8) << "Payment" << setw(9) << "Interest" << setw(11) << "New Balance" << endl;
+    std::cout << left << setw(6) << "Month" << setw(8) << "Balance" << setw(8) << "Payment" << setw(9) << "Interest" << setw(11) << "New Balance" << endl;
     std::cout << setw(42) << setfill('-') << "" << setfill(' ') << endl;
+    std::cout << setw(6) << "1" << setw(8) << "$" << loanAmount << setw(8) << "$0.00" << setw(9) << "$0.00" << setw(11) << "$" << loanAmount << endl;
 
-    std::cout << setw(6) << "1" << setw(8) << loanAmount << setw(8) << "payment" << setw(9) << interestRate << setw(11) << "new balance" << endl;
-    std::cout << setw(6) << "1" << setw(8) << loanAmount << setw(8) << "payment" << setw(9) << interestRate << setw(11) << "new balance" << endl;
-    std::cout << setw(6) << "1" << setw(8) << loanAmount << setw(8) << "payment" << setw(9) << interestRate << setw(11) << "new balance" << endl;
-    std::cout << setw(6) << "1" << setw(8) << loanAmount << setw(8) << "payment" << setw(9) << interestRate << setw(11) << "new balance" << endl;
-    std::cout << setw(6) << "1" << setw(8) << loanAmount << setw(8) << "payment" << setw(9) << interestRate << setw(11) << "new balance" << endl;
-    std::cout << setw(6) << "1" << setw(8) << loanAmount << setw(8) << "payment" << setw(9) << interestRate << setw(11) << "new balance" << endl;
-    std::cout << setw(6) << "1" << setw(8) << loanAmount << setw(8) << "payment" << setw(9) << interestRate << setw(11) << "new balance" << endl;
-    std::cout << setw(6) << "1" << setw(8) << loanAmount << setw(8) << "payment" << setw(9) << interestRate << setw(11) << "new balance" << endl;
-    std::cout << setw(6) << "1" << setw(8) << loanAmount << setw(8) << "payment" << setw(9) << interestRate << setw(11) << "new balance" << endl;
-    std::cout << setw(6) << "1" << setw(8) << loanAmount << setw(8) << "payment" << setw(9) << interestRate << setw(11) << "new balance" << endl;
-    std::cout << setw(6) << "1" << setw(8) << loanAmount << setw(8) << "payment" << setw(9) << interestRate << setw(11) << "new balance" << endl;
-    std::cout << setw(6) << "1" << setw(8) << loanAmount << setw(8) << "payment" << setw(9) << interestRate << setw(11) << "new balance" << endl;
-    std::cout << setw(6) << "1" << setw(8) << loanAmount << setw(8) << "payment" << setw(9) << interestRate << setw(11) << "new balance" << endl;
+    double newBalance = (loanAmount - paymentAmount) + ((loanAmount - paymentAmount) * interestRate);
+    double interestPayment = (loanAmount - paymentAmount) * interestRate;
+
+    for (int monthIndex = 1; monthIndex < 11; ++monthIndex)
+    {
+        /*while (monthIndex < 12)
+        {
+            
+        };*/
+
+        newBalance = (loanAmount - paymentAmount) + ((loanAmount - paymentAmount) * interestRate);
+        double statementBalance = newBalance;
+
+        std::cout << setw(6) << monthIndex + 1 << setw(8) << "$" << statementBalance << setw(8) << "$" << paymentAmount << setw(9) << "$" << interestPayment << setw(11) << "$" << newBalance << endl;
+
+    };
+
+    /*std::cout << setw(6) << "1" << setw(8) << loanAmount << setw(8) << "payment" << setw(9) << interestRate << setw(11) << "new balance" << endl;
+    std::cout << setw(6) << "2" << setw(8) << loanAmount << setw(8) << "payment" << setw(9) << interestRate << setw(11) << "new balance" << endl;
+    std::cout << setw(6) << "3" << setw(8) << loanAmount << setw(8) << "payment" << setw(9) << interestRate << setw(11) << "new balance" << endl;
+    std::cout << setw(6) << "4" << setw(8) << loanAmount << setw(8) << "payment" << setw(9) << interestRate << setw(11) << "new balance" << endl;
+    std::cout << setw(6) << "5" << setw(8) << loanAmount << setw(8) << "payment" << setw(9) << interestRate << setw(11) << "new balance" << endl;
+    std::cout << setw(6) << "6" << setw(8) << loanAmount << setw(8) << "payment" << setw(9) << interestRate << setw(11) << "new balance" << endl;
+    std::cout << setw(6) << "7" << setw(8) << loanAmount << setw(8) << "payment" << setw(9) << interestRate << setw(11) << "new balance" << endl;
+    std::cout << setw(6) << "8" << setw(8) << loanAmount << setw(8) << "payment" << setw(9) << interestRate << setw(11) << "new balance" << endl;
+    std::cout << setw(6) << "9" << setw(8) << loanAmount << setw(8) << "payment" << setw(9) << interestRate << setw(11) << "new balance" << endl;
+    std::cout << setw(6) << "10" << setw(8) << loanAmount << setw(8) << "payment" << setw(9) << interestRate << setw(11) << "new balance" << endl;
+    std::cout << setw(6) << "11" << setw(8) << loanAmount << setw(8) << "payment" << setw(9) << interestRate << setw(11) << "new balance" << endl;
+    std::cout << setw(6) << "12" << setw(8) << loanAmount << setw(8) << "payment" << setw(9) << interestRate << setw(11) << "new balance" << endl;
+    std::cout << setw(6) << "13" << setw(8) << loanAmount << setw(8) << "payment" << setw(9) << interestRate << setw(11) << "new balance" << endl;
     */
 }   
