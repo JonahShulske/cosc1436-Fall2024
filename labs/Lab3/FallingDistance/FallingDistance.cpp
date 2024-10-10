7f598f7d0e6b4a814a748af3612b075e4024abfd
Jonah Shulske <jonah.shulske@my.tccd.edu>
2024-10-10 07:41:11 -0500

    Began working on Lab3 functions.

--- labs/Lab3/FallingDistance/FallingDistance.cpp
+++ labs/Lab3/FallingDistance/FallingDistance.cpp
@@ -4,19 +4,40 @@
 */
 
 #include <iostream>
+#include <string>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+void FallTimePrompt()
+{
+    int fallTime;
+    std::cout << "Enter Fall Time in Seconds (1-60): ";
+    cin >> fallTime;
+    
+    while (fallTime < 1 || fallTime > 60)
+    {
+        std::cout << "ERROR: Fall Time must be between 1-60 seconds." << endl;
+        std::cout << "Enter Fall Time in Seconds (1-60): ";
+        cin >> fallTime;
+    };
+
+    std::cout << fallTime;
+}
+
+void MeterCalculation()     //      (1/2) * gt^2
+{
+
+    //double meterCalculation = (0.5 * (9.8 * sqrt(fallTime));
+    double fallDistanceMeters;
+
+
+
+
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    FallTimePrompt();
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
