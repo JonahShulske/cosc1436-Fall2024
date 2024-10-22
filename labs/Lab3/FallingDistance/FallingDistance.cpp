fccd2e3c1cba808cadb0e8b3c39d0d404aba7598
Jonah Shulske <jonah.shulske@my.tccd.edu>
2024-10-22 08:59:26 -0500

    Trying to figure out calculation for meters. Completely confused on functions.

--- labs/Lab3/FallingDistance/FallingDistance.cpp
+++ labs/Lab3/FallingDistance/FallingDistance.cpp
@@ -10,57 +10,88 @@
 
 using namespace std;
 
-//int ReadInt(string message, int minValue, int maxValue)
+int ReadInt(string message, int minValue, int maxValue)
+{
+    int value;
+    do 
+    {
+        std::cout << message;
+        cin >> value;
+
+        if (value < minValue || value > maxValue)
+        {
+            std::cout << "ERROR: Value must be between " << minValue << "-" << maxValue << endl;
+        }
+
+    } while (value < minValue || value > maxValue);
+    return value;
+}
+
+int ReadInt(string message)
+{
+    std::cout << message;
+    int value;
+    cin >> value;
+    return value;
+}
+
+void FallTimeCalculation(string message)
+{
+    double fallTime = ReadInt(message);
+    double fallTimeMeters = (0.5 * (9.8 * sqrt(fallTime)));
+
+    std::cout << fallTime << endl;
+    std::cout << fallTimeMeters << endl;
+}
+
+void Table(string message)
+{
+    std::cout << left << setw(7) << "Seconds" << setw(8) << "Distance" << endl;
+    std::cout << left << setw(20) << setfill('=') << "" << setfill(' ') << endl;
+    for (int secondsIndex; secondsIndex < ReadInt(message), ++secondsIndex)
+    {
+        std::cout << setw(7) << secondsIndex + 1 << 
+
+    }
+}
+
+///// @brief Prompts user for Fall Time
+//int FallTimePrompt(int fallTime)
 //{
-//    int input;
-//    do 
+//    /*int fallTime;
+//    fallTime = ReadInt("Enter Fall Time in Seconds (1-60): ", 1, 60);*/
+//
+//    std::cout << "Enter Fall Time in Seconds (1-60): ";
+//    cin >> fallTime;
+//    
+//    while (fallTime < 1 || fallTime > 60)
 //    {
-//        std::cout << message;
-//        cin >> input;
+//        std::cout << "ERROR: Fall Time must be between 1-60 seconds." << endl;
+//        std::cout << "Enter Fall Time in Seconds (1-60): ";
+//        cin >> fallTime;
+//    };
 //
-//        if (input < minValue || input > maxValue)
-//        {
-//            std::cout << "ERROR: Value must be between " << minValue << " - " << maxValue << endl;
-//        }
-//
-//    } while (minValue < 1 || maxValue > 60);
-//    return input;
+//    return fallTime;
+//    std::cout << fallTime;
 //}
 
-/// @brief Prompts user for Fall Time
-void FallTimePrompt()
-{
-    /*int fallTime;
-    fallTime = ReadInt("Enter Fall Time in Seconds (1-60): ", 1, 60);*/
-
-    int fallTime;
-    std::cout << "Enter Fall Time in Seconds (1-60): ";
-    cin >> fallTime;
-    
-    while (fallTime < 1 || fallTime > 60)
-    {
-        std::cout << "ERROR: Fall Time must be between 1-60 seconds." << endl;
-        std::cout << "Enter Fall Time in Seconds (1-60): ";
-        cin >> fallTime;
-    };
-
-    std::cout << fallTime;
-}
-
-/// @brief Calculates Fall Time in Meters
-void MeterCalculation()     //      (1/2) * gt^2
-{
-
-    //double meterCalculation = (0.5 * (9.8 * sqrt(fallTime));
-    double meterCalculation = (0.5 * sqrt(9.8 * fallTime));
-    double fallDistanceMeters;
-
-    cout << meterCalculation;
-
-}
+///// @brief Calculates Fall Time in Meters
+//int MeterCalculation(int fallTime)     //      (1/2) * gt^2
+//{
+//
+//    //double meterCalculation = (0.5 * (9.8 * sqrt(fallTime));
+//    double meterCalculation = (0.5 * (9.8 * sqrt(fallTime)));
+//    double fallDistanceMeters;
+//
+//    return meterCalculation;
+//    cout << meterCalculation;
+//    
+//}
 
 int main()
 {
-    FallTimePrompt()
-    MeterCalculation()
+    int value = ReadInt("Enter Fall Time in Seconds (1-60): ", 1, 60);
+    /*FallTimePrompt();
+    MeterCalculation();
+    */
 }
