aa77c65a3b94f07b3ff7136b094b7d7046ff5e6d
Jonah Shulske <jonah.shulske@my.tccd.edu>
2024-10-29 08:42:28 -0500

    Possible final build? Table is functioning, switch statement now requires correct input, etc.

--- labs/Lab3/FallingDistance/FallingDistance.cpp
+++ labs/Lab3/FallingDistance/FallingDistance.cpp
@@ -10,16 +10,9 @@
 
 using namespace std;
 
-//int ReadInt(string message, int minValue, int maxValue)
-//{
-//    int value;
-//    std::cout << message;
-//    cin >> value;
-//
-//    return value;
-//}
-
-/// @brief Prompts user for Fall Time
+/// @brief Prompts user for Fall Time and retrives input
+/// @param message Message displaying request for fallTime input
+/// @return fallTime integral value entered by user
 int FallTimePrompt()
 {
     int fallTime;
@@ -33,122 +26,86 @@ int FallTimePrompt()
         std::cout << "Enter Fall Time in Seconds (1-60): ";
         cin >> fallTime;
     };
-
     return fallTime;
 }
 
+/// @brief Calculates Fall Distance in Meters
+/// @param seconds Amount of seconds an object will be falling
+/// @return Calculation of fallen meters per second
 double DistanceMeters(int seconds)
 {
-    
     double fallDistanceMeters = (9.8 * pow(seconds, 2) / 2);
 
     return fallDistanceMeters;
-   /* return fallDistanceFeet;*/
 }
 
+/// @brief Calculates Fall Distance in Feet
+/// @param seconds Amount of seconds an object will be falling
+/// @return Calculation of fallen feet per second
 double DistanceFeet(int seconds)
 {
     double fallDistanceMeters = DistanceMeters(seconds);
     double fallDistanceFeet = (fallDistanceMeters * 3.28084);
 
-    cout << fixed << setprecision(2) << fallDistanceFeet << endl;
-
     return fallDistanceFeet;
 }
 
-//int ReadInt(string message, int minValue, int maxValue)
-//{
-//    int value;
-//    do 
-//    {
-//        std::cout << message;
-//        cin >> value;
-//
-//        if (value < minValue || value > maxValue)
-//        {
-//            std::cout << "ERROR: Value must be between " << minValue << "-" << maxValue << endl;
-//        }
-//
-//    } while (value < minValue || value > maxValue);
-//    return value;
-//}
-
-//int ReadInt(string message)
-//{
-//    std::cout << message;
-//    int value;
-//    cin >> value;
-//    return value;
-//}
-
+/// @brief Prompts user for a calculation in Meters or Feet
+/// @return User's entry for measurment unit
 bool MetersOrFeet()
 {
-    char measuringUnit;
     bool done = false;
     while (!done)
     {
         char measuringUnit;
-        
         std::cout << "Do you want to calculate in Meters or Feet? (M/F): ";
         cin >> measuringUnit;
         
         switch (measuringUnit)
         {
-            case 'M':
+            case 'M': measuringUnit = true; done = true; break;
             case 'm': measuringUnit = true; done = true; break;
 
-            case 'F':
+            case 'F': measuringUnit = false; done = true; break;
             case 'f': measuringUnit = false; done = true; break;
 
-            default:
+            default:;
             {
                 std::cout << "ERROR: Enter (M/F) characters only" << endl;
             }
-
+            continue;
         }
         return measuringUnit;
     };
-
+    cin.ignore();
 }
 
+/// @brief Displays table of all previous information collected
 void DisplayTable()
 {
     int fallTime = FallTimePrompt();
     bool metersOrFeet = MetersOrFeet();
-   /* double fallDistanceMeters = DistanceMeters();
-    double fallDistanceFeet = DistanceFeet();*/
 
     std::cout << left << setw(7) << "Seconds " << setw(8) << " Distance" << endl;
     std::cout << left << setw(20) << setfill('=') << "" << setfill(' ') << endl;
 
     if (metersOrFeet == true)
     {
-        /*std::cout << setw(9) << "1" << setw(8) << fallDistanceMeters << endl;*/
-
-        for (int secondsIndex = 0; secondsIndex < fallTime; ++secondsIndex)
+        for (int secondsIndex = 1; secondsIndex <= fallTime; ++secondsIndex)
         {
-            std::cout << setw(9) << secondsIndex + 1 << setw(8) << fixed << setprecision(2) << DistanceMeters(secondsIndex) << endl;
+            std::cout << setw(9) << secondsIndex << setw(2) << fixed << setprecision(2) << DistanceMeters(secondsIndex) << " m" << endl;
 
         }
     } else if (metersOrFeet == false)
     {
-        for (int secondsIndex = 0; secondsIndex < fallTime; ++secondsIndex)
+        for (int secondsIndex = 1; secondsIndex <= fallTime; ++secondsIndex)
         {
-            std::cout << setw(9) << secondsIndex + 1 << setw(8) << fixed << setprecision(2) << DistanceFeet(secondsIndex) << endl;
+            std::cout << setw(9) << secondsIndex << setw(2) << fixed << setprecision(2) << DistanceFeet(secondsIndex) << " ft" << endl;
         }
     }
-
-
 }
-
 
 int main()
 {
     DisplayTable();
-
-    //int fallTime = ReadInt("Enter Fall Time in Seconds (1-60): ", 1, 60);
-    
-    
-    
-   /* Table();*/
 }
