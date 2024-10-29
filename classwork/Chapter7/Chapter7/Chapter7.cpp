88a71f9d0c7917411e493e7667f7a779fbc2c923
Jonah Shulske <jonah.shulske@my.tccd.edu>
2024-10-28 21:23:27 -0500

    Added onto Chapter7 notes, working with Arrays and Bounds to help prevent security issues.

--- classwork/Chapter7/Chapter7/Chapter7.cpp
+++ classwork/Chapter7/Chapter7/Chapter7.cpp
@@ -1,5 +1,5 @@
 /* Chapter 7 - Pass By Reference
- * 10/23/24 
+ * 10/23/24 + 10/28/24
  */
 
 #include <iostream>
@@ -64,38 +64,100 @@ using namespace std;
 //    Translate(rc, 5, 10);
 //    Display(rc);
 //}
+void GradeArrayDemo()
+{
+    const int maxGrades = 20;
+    int grades[maxGrades];
+    double dummyValue2 = -1;
+    int dummyValue = 0;
+
+    int estimatedMaxArraySize = sizeof(grades) / sizeof(int); // Size of array / size of element. Only works if used on array declared in same function. Unreliable. Doesn't work at runtime
+        
+    int count = 0;
+    for (int index = 0; index < maxGrades; ++index/*, ++count*/)
+    {
+        std::cout << "Enter a grade: ";
+        cin >> grades[index];
+        /*grades[index] = 100;*/
+        if (grades[index] <= 0)
+            break;
+
+        ++count;
+    };
+
+    for (int index = 0; index < count; ++index)
+    {
+        std::cout << grades[index] << endl;
+
+    };
+
+    int indexToChange;
+    do 
+    {
+        std::cout << "Enter the index of the grade to change: ";
+        cin >> indexToChange;
+
+    } while (indexToChange < 0 || indexToChange >= maxGrades);
+
+    int newGrade;
+    std::cout << "Enter the new grade: ";
+    cin >> newGrade;
+
+    grades[indexToChange] = newGrade;
+}
+
+void InitArrayDemo() 
+{
+    const int MaxRates = 100;
+    double payRates[MaxRates] = {0}; // Zero Initialized - Tells compiler to generate code to assign value 0 to each of the array's elements.
+
+    /*for (int index = 0; index < MaxRates; ++index)
+    {
+        payRates[index] = 0;
+    };*/
+}
 
 int main()
 {
+    double gradeAverages[100];
+    GradeArrayDemo();
+    InitArrayDemo();
+    
+
+
+
     /* Array - set of related data
      * Used when you need to store more than one value of a type. Indiviual values called elements. Index starts at 0
      * Syntax known as "Array access operator"
-     * Elements are same type 
+     * Elements are same type
      */
-    const int MaxStudents = 100;
-    string students[MaxStudents]; // Must have int value and be compile time complicit
 
-    // Store roster of students
-    // When using for loop, use = 0 to < N
-    for (int index = 0; index < MaxStudents; ++index)
-    {
-        string student;
-        std::cout << "Enter student name: ";
-        getline(cin, student);
-        if (student == "")
-            break;
+   
 
-        /* Student index at 0, first element
-         * students sub 0
-         */
-        students[index] = student;
-    };
+    //const int MaxStudents = 100;
+    //string students[MaxStudents]; // Must have int value and be compile time complicit
 
-    // Print roster
-    for (int index = 0; index < MaxStudents; ++index)
-    {
-        if (students[index] != "")
-            std::cout << students[index] << endl;
-    };
+    //// Store roster of students
+    //// When using for loop, use = 0 to < N
+    //for (int index = 0; index < MaxStudents; ++index)
+    //{
+    //    string student;
+    //    std::cout << "Enter student name: ";
+    //    getline(cin, student);
+    //    if (student == "")
+    //        break;
+
+    //    /* Student index at 0, first element
+    //     * students sub 0
+    //     */
+    //    students[index] = student;
+    //};
+
+    //// Print roster
+    //for (int index = 0; index < MaxStudents; ++index)
+    //{
+    //    if (students[index] != "")
+    //        std::cout << students[index] << endl;
+    //};
 
 }
