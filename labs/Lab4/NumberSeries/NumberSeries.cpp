50fdd0130edffc37b297de71be5b13dc1a800511
Jonah Shulske <jonah.shulske@my.tccd.edu>
2024-10-31 08:01:41 -0500

    Created Lab4/NumberSeries program

---
+++ labs/Lab4/NumberSeries/NumberSeries.cpp
@@ -0,0 +1,16 @@
+/* NumberSeries
+ * Jonah Shulske
+ * COSC-1436 Fall 2024
+ */
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
