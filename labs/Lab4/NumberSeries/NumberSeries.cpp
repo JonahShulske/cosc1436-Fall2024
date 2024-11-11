d1cd975dbd3d94c9fd95ab635276edeb9da78992
Jonah Shulske <jonah.shulske@my.tccd.edu>
2024-11-11 10:14:51 -0600

    Constructed main loop and created Main Menu. However, I don't know how to prompt the main menu without a global variable since the Chapter6 notes with the MovieLibrary was cut short too early.

--- labs/Lab4/NumberSeries/NumberSeries.cpp
+++ labs/Lab4/NumberSeries/NumberSeries.cpp
@@ -10,25 +10,90 @@
 
 using namespace std;
 
+enum MenuCommand
+{
+    MC_Add = 1,
+    MC_Longest,
+    MC_Mean,
+    MC_Smallest,
+    MC_Insert,
+    MC_View,
+    MC_Quit,
+};
+
 void SeriesArray()
 {
     const int maxNumberAmount = 100;
     int numberSeriesArray[maxNumberAmount];
 
-    for (int index = 0; index > maxNumberAmount; ++index)
+    int count = 0;
+    for (int index = 0; index < maxNumberAmount; ++index)
     {
-        int numberSeries;
         std::cout << "Enter a value: ";
-        cin >> numberSeries;
+        cin >> numberSeriesArray[index];
 
-        if (numberSeries <= 0)
+        while (numberSeriesArray[index] < 0)
+        {
+            std::cout << "ERROR: Value must be greater than or equal to 0" << endl;
+            std::cout << "Enter a value: ";
+            cin >> numberSeriesArray[index];
+        }
+
+        if (numberSeriesArray[index] <= 0)
             break;
-    }
+
+        ++count;
+    };
 }
+
+//void DisplayMenu(MenuCommand menuCommand)
+//{
+//    std::cout << "Main Menu" << endl;
+//    std::cout << left << setw(15) << setfill('-') << "" << setfill(' ') << endl;
+//    std::cout << "A) dd " << endl;
+//    std::cout << "L) argest " << endl;
+//    std::cout << "M) ean " << endl;
+//    std::cout << "S) mallest " << endl;
+//    std::cout << "I) nsert " << endl;
+//    std::cout << "V) iew " << endl;
+//    std::cout << "Q) uit " << endl;
+//    std::cout << "Enter Menu Choice: ";
+//
+//    do
+//    {
+//        char input;
+//        cin >> input;
+//
+//        switch (input)
+//        {
+//            case 'A':
+//            case 'a': menuCommand = MenuCommand::MC_Add; break;
+//
+//            case 'L':
+//            case 'l': menuCommand = MenuCommand::MC_Longest; break;
+//
+//            case 'M':
+//            case 'm': menuCommand = MenuCommand::MC_Mean; break;
+//
+//            case 'S':
+//            case 's': menuCommand = MenuCommand::MC_Smallest; break;
+//
+//            case 'I':
+//            case 'i': menuCommand = MenuCommand::MC_Insert; break;
+//
+//            case 'V':
+//            case 'v': menuCommand = MenuCommand::MC_View; break;
+//
+//            case 'Q':
+//            case 'q': menuCommand = MenuCommand::MC_Quit; break;
+//        };
+//    } while (menuCommand == 0);
+//
+//    cin.ignore();
+//}
 
 int main()
 {
     SeriesArray();
-
-   
+    /*DisplayMenu();*/
 }
