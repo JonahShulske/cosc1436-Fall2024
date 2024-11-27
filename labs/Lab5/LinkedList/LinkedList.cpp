b2ff54a5c1ed5b991e53492672a5d027431777f9
Jonah Shulske <jonah.shulske@my.tccd.edu>
2024-11-27 17:03:53 -0600

    Thought I was getting it, but I'm lost again. Main menu kinda works?

--- labs/Lab5/LinkedList/LinkedList.cpp
+++ labs/Lab5/LinkedList/LinkedList.cpp
@@ -19,72 +19,84 @@ enum MenuCommand
     MC_Quit
 };
 
-struct Node
-{
-    int value = 0;
-
-    Node* Next = nullptr;
-};
+//struct Node
+//{
+//    int value = 0;
+//    int Current;
+//
+//    Node* Next = nullptr;
+//};
 
 struct LinkedList
 {
-    Node* Head = nullptr;
+    LinkedList* Next = nullptr;
+    LinkedList* Head = nullptr;
 };
 
-void AddList()
+void AddFunction()
 {
-    LinkedList Head, list;
-    Node* current = list.Head;
-    int Value;
-
-    std::cout << "Enter value to add to list: ";
-    cin >> Value;
-
+    LinkedList list = new Node;
+    LinkedList* current = list.Head;
     while (current != nullptr && current->Next != nullptr)
         current = current->Next;
 }
 
-//void DisplayMenu(MenuCommand menuCommand)
-//{
-//    char input;
-//    std::cout << "Main Menu" << endl;
-//    std::cout << left << setw(15) << setfill('-') << "" << setfill(' ') << endl;
-//    std::cout << "L) ist " << endl;
-//    std::cout << "A) dd" << endl;
-//    std::cout << "D) elete " << endl;
-//    std::cout << "C) lear " << endl;
-//    std::cout << "Q) uit " << endl;
-//    std::cout << "Enter Menu Choice: ";
-//    cin >> input;
-//
-//    do
-//    {
-//        switch (input)
-//        {
-//            case 'A':
-//            case 'a': menuCommand = MenuCommand::MC_Add; break;
-//
-//            case 'L':
-//            case 'l': menuCommand = MenuCommand::MC_List; break;
-//
-//            case 'D':
-//            case 'd': menuCommand = MenuCommand::MC_Delete; break;
-//
-//            case 'C':
-//            case 'c': menuCommand = MenuCommand::MC_Clear; break;
-//
-//            case 'Q':
-//            case 'q': menuCommand = MenuCommand::MC_Quit; break;
-//
-//            default: std::cout << "ERROR: Bad Input" << endl; break;
-//        };
-//    } while (menuCommand == 0);
-//
-//    cin.ignore();
-//}
+void DisplayMenu(MenuCommand menuCommand)
+{
+   
+    std::cout << "Main Menu" << endl;
+    std::cout << left << setw(15) << setfill('-') << "" << setfill(' ') << endl;
+    std::cout << "L) ist " << endl;
+    std::cout << "A) dd" << endl;
+    std::cout << "D) elete " << endl;
+    std::cout << "C) lear " << endl;
+    std::cout << "Q) uit " << endl;
+    std::cout << "Enter Menu Choice: ";
+
+    do
+    {
+        char input;
+        cin >> input;
+
+        switch (input)
+        {
+            case 'A':
+            case 'a': menuCommand = MenuCommand::MC_Add; break;
+
+            case 'L':
+            case 'l': menuCommand = MenuCommand::MC_List; break;
+
+            case 'D':
+            case 'd': menuCommand = MenuCommand::MC_Delete; break;
+
+            case 'C':
+            case 'c': menuCommand = MenuCommand::MC_Clear; break;
+
+            case 'Q':
+            case 'q': menuCommand = MenuCommand::MC_Quit; break;
+
+            default: std::cout << "ERROR: Bad Input" << endl;
+        };
+    } while (menuCommand == 0);
+
+    cin.ignore();
+}
+
+void HandleMovie(MenuCommand menuCommand)
+{
+    switch (menuCommand)
+    {
+        case MenuCommand::MC_Add: AddFunction(); break;
+        /*case MenuCommand::MC_List: ListFunction(); break;
+        case MenuCommand::MC_Delete: DeleteFunction(); break;
+        case MenuCommand::MC_Clear: ClearFunction(); break;
+        case MenuCommand::MC_Quit: QuitFunction(); break;*/
+    };
+}
 
 int main()
 {
-    /*MenuCommand menuCommand;
-    DisplayMenu(menuCommand);*/
+    MenuCommand menuCommand = (MenuCommand)0;
+    DisplayMenu(menuCommand);
+    HandleMovie(menuCommand);
 }
